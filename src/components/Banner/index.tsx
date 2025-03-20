c368768ff6fd93a092a45f40bcc66cdb224875cf
Tim Ellison <tellison@users.noreply.github.com>
2025-03-20 14:52:00 +0000

    Update banner text
    Minor update, this is the first GA release and not a PSU.

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
    return (
      <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
        <strong className='p-1'>19th March 2025:</strong>
-          We are creating the March 2025 PSU binaries for Eclipse Temurin 24+36<br/>
+          We are creating the March 2025 binaries for Eclipse Temurin 24+36<br/>
           You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/76">by platform</a>.
        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
      </div>
