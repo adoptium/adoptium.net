50ab63b1d5b30458e0bba5e520a53223cc1d50be
Sophia Guo <sophia.gwf@gmail.com>
2025-03-31 12:09:51 -0400

    Remove March 2025 release banner
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,14 +6,14 @@ const Banner = () => {
   // The following is an example that can be used for future banner alert
   // Comment Out The Above Line ( return null ; ) and uncomment the below
 
-   return (
-     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-       <strong className='p-1'>19th March 2025:</strong>
-          We are creating the March 2025 binaries for Eclipse Temurin 24+36<br/>
-          You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/76">by platform</a>.
-       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-     </div>
-   );
+  // return (
+  //   <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+  //     <strong className='p-1'>19th March 2025:</strong>
+  //       We are creating the March 2025 binaries for Eclipse Temurin 24+36<br/>
+  //        You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/76">by platform</a>.
+  //     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  //   </div>
+  // );
 };
 
 export default Banner;
