a8fceea6cfcb5c0f16bccbd5f5f442bd4c0565ad
George Adams <georgeadams1995@gmail.com>
2025-06-16 16:24:04 +0100

    ci: fix test issue where date.Now changes

--- src/app/[locale]/temurin/nightly/__tests__/page.test.tsx
+++ src/app/[locale]/temurin/nightly/__tests__/page.test.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import { render } from "@testing-library/react"
-import { describe, expect, it } from "vitest"
+import { describe, expect, it, vi } from "vitest"
 import { axe } from "vitest-axe"
 import TemurinNightly from "../page"
 
 describe("TemurinNightly page", () => {
   it("renders correctly", () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date("2025-06-13T12:00:00Z"));
     const { container } = render(<TemurinNightly />)
     expect(container.firstChild).toMatchSnapshot()
+    vi.useRealTimers();
   })
 
   it("has no accessibility violations", async () => {
