cf7f3e5ed4144004efba755bf0b55cf21a2d1fa8
George Adams <georgeadams1995@gmail.com>
2025-06-17 13:11:16 +0100

    convert gtag/GA to use @next/third-parties/google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fortawesome/fontawesome-free/css/v4-shims.min.css'
 import { Inter } from 'next/font/google'
 import { Metadata } from 'next'
-import Script from 'next/script'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import { getLocale } from 'next-intl/server'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -57,41 +57,15 @@ export default async function RootLayout({
   // Get locale dynamically - this will work for both the root path (/)
   // and localized paths (/[locale])
   const locale = await getLocale();
-  
+
   return (
     <html lang={locale}>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5WLCZXC');
-          `}
-        </Script>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-9HHPS5RX9D"
-        />
-        <Script
-          id="gtag-inline-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-9HHPS5RX9D', {
-                anonymize_ip: true
-              });
-            `,
-          }}
-        />
+        <GoogleTagManager gtmId="GTM-5WLCZXC" />
+        <GoogleAnalytics gaId="G-9HHPS5RX9D" />
       </head>
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) - used for people who have JavaScript disabled */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5WLCZXC"
@@ -100,7 +74,7 @@ export default async function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-          {children}
+        {children}
       </body>
     </html>
   )
