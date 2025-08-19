cd2b3c6f2204220696e60b3f697a0f42dbcb021d
Daniel Ntege <danientege785@gmail.com>
2025-08-19 11:21:49 +0300

    Corrections on the /sponsors redirection to /sustainers (#285)
    * test: add unit test for Sponsers Page redirect functionality
    
    * Change from sponsers to sponsors
    
    * Also changing the tests
    
    * refactored the redirection using the config file
    
    * more on redirection
    
    * commit
    
    ---------
    
    Co-authored-by: Xavier FACQ <xavierfacq@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:locale/sponsors",
+        destination: "/:locale/sustainers",
+        permanent: true,
+      }
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
