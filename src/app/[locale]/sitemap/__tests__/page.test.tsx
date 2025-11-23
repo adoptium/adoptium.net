c1257a330bb8436ba3b35d0dedd6400fdd5de3e5
Xavier FACQ <xavierfacq@users.noreply.github.com>
2025-11-23 21:52:06 +0100

    Fix/pb with banner and date (#545)
    * Use fake date to avoid pb of build with Banner elements
    
    * Fix all dates for Banners

--- src/app/[locale]/sitemap/__tests__/page.test.tsx
+++ src/app/[locale]/sitemap/__tests__/page.test.tsx
@@ -7,12 +7,18 @@ import { SitemapData } from '@/types/sitemap';
 describe('SitemapClient', () => {
   beforeEach(() => {
     vi.clearAllMocks();
-    vi.useFakeTimers();
+
+    // Default date for all tests -> 21/12/2012 - This is the end of the world!
+    // NOTE: this is important for accessibility tests
+    const date = Date.UTC(2012, 11, 21, 0, 0, 0, 0);
+    vi.useFakeTimers({now: date, shouldAdvanceTime: true});
+
     document.body.innerHTML = '';
   });
 
   afterEach(() => {
     vi.useRealTimers();
+
     document.body.innerHTML = '';
   });
 
--- src/components/Banner/__test__/index.test.tsx
+++ src/components/Banner/__test__/index.test.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
-import { describe, it, expect, afterEach, vi } from "vitest";
+import { describe, it, expect, afterEach, vi, beforeEach } from "vitest";
 import Banner from "../index";
 
-vi.useFakeTimers();
-
 describe("Banner", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    // Default date for all tests -> 21/12/2012 - This is the end of the world!
+    // NOTE: this is important for accessibility tests
+    const date = Date.UTC(2012, 11, 21, 0, 0, 0, 0);
+    vi.useFakeTimers({now: date, shouldAdvanceTime: true});
+  });
+
   afterEach(() => {
     cleanup();
+
+    vi.useRealTimers();
   });
 
   it("renders the banner with heading", () => {
