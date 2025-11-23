90270f6d49930424da1623ef3142bbddf89db25c
Xavier FACQ <xavier.facq@free.fr>
2025-11-23 16:34:22 +0100

    Use fake date to avoid pb of build with Banner elements

--- src/components/BannerMiddle/__test__/index.test.tsx
+++ src/components/BannerMiddle/__test__/index.test.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
-import { describe, it, expect, afterEach, vi } from "vitest";
+import { describe, it, expect, afterEach, vi, beforeEach } from "vitest";
 import BannerMiddle from "../index";
 
 vi.useFakeTimers();
 
 describe("BannerMiddle", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    vi.useFakeTimers();
+
+    // Default date for all tests -> 21/12/2012 - This is the end of the world!
+    // NOTE: this is important for accessibility tests
+    const date = Date.UTC(2012, 11, 21, 0, 0, 0, 0);
+    vi.setSystemTime(date);
+  });
+
   afterEach(() => {
     cleanup();
+
+    vi.useRealTimers();
   });
 
   it("renders the banner with heading", () => {
